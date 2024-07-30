ca7c7b67ca3225de17e8813aa4511a1643a1a10c
Shashikant Prasad <shashikantprasad107@gmail.com>
2024-07-31 04:55:04 +0530

    add: client setup files

---
+++ client/vite.config.js
@@ -0,0 +1,16 @@
+import path from "path"
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
