07b19d78e193761e514819497d0467107624ef4d
Shashikant Prasad <shashikantprasad107@gmail.com>
2024-08-05 12:31:51 +0530

    added: redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,15 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import AppRouter from "./AppRouter";
 import { Provider } from "react-redux";
-import store from "./store/store";
+import store, { persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={AppRouter} />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={AppRouter} />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,15 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore, FLUSH, PAUSE, PERSIST, PURGE, REHYDRATE, REGISTER } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import videoReducer from "./videoSlice";
 import uiReducer from "./uiSlice";
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    videos: videoReducer,
-    ui: uiReducer
+const persistConfig = {
+  key: "root",
+  storage
+}
 
-  }
+const rootReducer = combineReducers({
+  auth: authReducer,
+  videos: videoReducer,
+  ui: uiReducer
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+})
+
+export const persistor = persistStore(store)
 export default store
